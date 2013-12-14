a18b259d84c201c9da5750b9235dd1e14d717282
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2013-12-14 15:14:33 +0200

    First version compiles

--- LudumDare/Main.cpp
+++ LudumDare/Main.cpp
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+#include <SFML/Graphics.hpp>
+
 int main(){
 	printf("Hello world");
+
+	sf::RenderWindow window(sf::VideoMode(800,600), "Ludum");
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::White);
+
+	while( window.isOpen() )
+	{
+		sf::Event event;
+
+		while(window.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+
+	}
 
 	return 0;
 }
